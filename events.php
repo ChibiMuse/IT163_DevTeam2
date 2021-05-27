8ba834fc40c41efa872783fa90daea05a9e9f031
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-05-26 17:11:28 -0700

    Test Website from Kelly

---
+++ events.php
@@ -0,0 +1,21 @@
+<?php include 'includes/header162v4.php'?>
+
+<h2>EVENTS PAGE</h2>
+<div class="container">
+    <section>
+    
+      section
+    
+
+    </section>
+    
+    <main> 
+    
+   main
+
+    </main>
+
+
+    <!-- END container -->
+    </div>
+    <?php include 'includes/footer163.php'?>
---
+++ includes/footer163.php
@@ -0,0 +1,41 @@
+
+
+</div>
+<footer>
+  <p>TBD<a href="contact.php" target="_blank"> Contact US</a><small>&copy; 2021-<?=date('Y')?><a href="mailto:1306homebase@gmail.com">All Rights Reserved</a></p>
+
+</footer>
+
+</div>
+<!-- END WRAPPER --> 
+</div>
+
+
+<!-- THESE HAVE TO DO WITH REPSONIVE NAVIGATION, NEED TO BE CHECKED. -->
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+  <!-- <script>
+    function myNavFunction() {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+
+}
+
+</script>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  } 
+}
+</script> -->
+
+</body>
+</html>
