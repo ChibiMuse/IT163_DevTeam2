3a2a31777984311849a8b7ed735df9011f60adfc
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-05-26 18:14:43 -0700

    Skeleton Page
    Here are the pages that need to have add the HTML to be ready for styling. 
    At this point it all seems to be functioning but if anything breaks check the link and names. 
    
    Have fun!

---
+++ finalpage_163/includes/header162v4.php
@@ -0,0 +1,45 @@
+<?php include 'includes/163configv4.php'?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title><?=$title?></title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8"/>
+
+<
+
+<!-- IS THIS THE RIGHTLINK TO bootstrap
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script> -->
+
+</head>
+
+<body>
+
+<div class="wrapper">
+
+<div class="mobinav" id="mobi">
+  <div id="myLinks">
+  <!-- Navigation links (hidden by default) -->
+    <!-- <a href="indexv4.php">HOME</a>
+    <a href="events.php">EVENTS</a>
+    <a href="resourcecs163.php">RESOURCES</a>
+    <a href="contact.php">CONTACT US</a> -->
+    
+  </div>
+  
+<header>
+<nav>
+    <ul class="topnav" id="myTopnav">
+      <?=makeLinks($nav1)?>
+            <!-- <li><a href="indexv4.php" class="selected" >HoOME</a></li>
+            <li><a href="resourcecs163.php">RESOURCES</a></li>
+            <li><a href="events.php">EVENTS</a></li>
+            <li><a href="contact.php">CONTACT US</a></li> -->
+      </ul>
+</nav>  
+
+</header>
