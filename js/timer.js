9072e11744fdcfcb0f6bea206b7b6dd4042470b6
jordanlui44 <59802861+jordanlui44@users.noreply.github.com>
2021-06-05 17:11:25 -0700

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,6 @@
-
+//You'll need this code to display it: <p>You've been here for <span id="minutes">00</span> minutes and <span id="seconds">00</span> seconds!</p>
+//<script src="js/timer.js"></script>
+//This function will count up the amount of time a user spends on a specific webpage.
 var minutesLabel = document.getElementById("minutes");
         var secondsLabel = document.getElementById("seconds");
         var totalSeconds = 0;
